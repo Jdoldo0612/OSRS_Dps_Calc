3d7dc2b3141bf0df9ae03361250bf115cacb25bd
UncleKratos <jdoldo0612@gmail.com>
2023-08-10 13:35:06 -0400

    Updates to max hit to be a function

--- Dps_Calc.py
+++ Dps_Calc.py
@@ -1,34 +1,39 @@
 import math
 
-# Constants
-Pray_BurstOfStr = 1.05
-Pray_SupHumStr = 1.1
-Pray_UltStr = 1.15
-Pray_Chiv = 1.18
-Pray_Piety = 1.23
+def CalcMaxHit(StrLevel, GearStrBonus, Prayer, CombatStyle, Potion):
+    prayerMultiplier = 0
+    CombatStyleBonus = 0
+    PotionBonus = 0
 
+    if(Prayer.lower() == "burst of strength"):
+        prayerMultiplier = 1.05
+    elif(Prayer.lower() == "superhuman strength"):
+        prayerMultiplier = 1.1
+    elif(Prayer.lower() == "ultimate strength"):
+        prayerMultiplier = 1.15
+    elif(Prayer.lower() == "chivalry"):
+        prayerMultiplier = 1.18
+    elif(Prayer.lower() == "piety"):
+        prayerMultiplier = 1.23
 
-# Defining functions for the potions as they are str level dependent
-def Pot_Str(strLvl):
-    return (math.floor(((strLvl * 0.1) + 2)))
+    if(CombatStyle.lower() == "controlled"):
+        CombatStyleBonus = 1
+    elif(CombatStyle.lower() == "aggressive"):
+        CombatStyleBonus = 3
 
+    if(Potion.lower() == "strength"):
+        PotionBonus = math.floor(((StrLevel * 0.1) + 2))
+    elif(Potion.lower() == "super strength"):
+        PotionBonus = math.floor(((StrLevel * 0.15) + 5))
+    elif(Potion.lower() == "overload"):
+        PotionBonus = 6 + math.floor(StrLevel * 0.16)
+    elif(Potion.lower() == "smelling salts"):
+        PotionBonus = 11 + math.floor(StrLevel * 0.16)
 
-def Pot_SupStr(strLvl):
-    return (math.floor(((strLvl * 0.15) + 5)))
+    effStrLvl = math.floor((StrLevel + PotionBonus) * prayerMultiplier) + CombatStyleBonus + 8
 
+    maxHit = math.floor(((effStrLvl * (64 + GearStrBonus)) + 320) / 640)
 
-# Melee
-pietyBoost = 1.23
-combatStyle = 1  # 1 if controlled, 3 if aggressive
+    return(maxHit)
 
-# verifying can match calcs, then move to interface after
-StrengthLevel = 99
-
-EffStrLvl = math.floor((StrengthLevel + Pot_SupStr(StrengthLevel)) * pietyBoost) + combatStyle + 8
-
-# Naked with whip rn for calc purposes
-GearStrBonus = 82
-
-MaxHit = math.floor(((EffStrLvl * (64 + GearStrBonus)) + 320) / 640)
-
-print(MaxHit)
+print(CalcMaxHit(99,82,"Piety","Controlled","Super Strength"))
