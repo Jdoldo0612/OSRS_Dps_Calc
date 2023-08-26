0ab13ccded590e545788fbc2021e1700a8c402f7
UncleKratos <jdoldo0612@gmail.com>
2023-08-26 07:33:51 -0400

    Initial revision

--- OsrsBox_Ifc.py
+++ OsrsBox_Ifc.py
@@ -3,5 +3,5 @@ from osrsbox import items_api
 all_db_items = items_api.load()
 
 for item in all_db_items:
-    if(item.name == "Dragon scimitar"):
+    if(item.name.lower() == "dragon scimitar"):
         print(item.name)
